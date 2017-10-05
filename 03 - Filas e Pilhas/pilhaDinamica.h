9d53e175821b9f3fd529c1c2914c99d5f34c3141
arthuevangelista <arthuevangelista@gmail.com>
2017-10-05 09:24:10 -0300

    att .h

--- 03 - Filas e Pilhas/pilhaDinamica.h
+++ 03 - Filas e Pilhas/pilhaDinamica.h
@@ -1,37 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define M 6
-
-struct pilha{
-	int init, final, qtd;
-	int dados[M];
-};
-
-typedef struct pilha Pilha;
 
 Pilha* init_pilha(Pilha* pi){
 /* Cria a pilha Dinâmica*/
-
   pi = NULL;
   return pi;
 
 }
 
-Pilha* cria_slot(){
-/* Cria a pilha Dinâmica*/
+Pilha* cria_slot(Pilha* l){
+/* Cria um slot da Pilha */
 
 	Pilha *pi  = (Pilha*)malloc(sizeof(struct pilha));
-	if(pi != NULL){
-    pi->val = 0;
-		pi->prox = NULL; // pi->prox = pi->topo
-    pi->ant = NULL; // quando desempilhar, anterior vira novo proximo
-	}
+  if(!(pilha_vazia(l))){
+  		pi->prox = NULL; // pi->prox = pi->topo
+      pi->ant = NULL; // quando desempilhar, anterior vira novo proximo
+  }else{
+      l = pi;
+  }
 	return pi;
 }
 
-void libera_pilha(Pilha* pi){
-/* Destrói a pilha Estática*/
-	free(pi);
+void libera_pilha(Pilha* fim){
+/* Destrói a pilha */
+	while(fim != NULL){
+    desempilha(fim, );
+  }
 }
 
 int pilha_vazia(Pilha* pi){
@@ -46,9 +40,9 @@ int pilha_vazia(Pilha* pi){
 
 /* Inserção em pilhas é sempre realizado no início da pilha. Também existe o caso da pilha estar vazia e ser necessário realizar uma inserção. OBS.: Devemos verificar se a pilha está cheia antes de inserir elementos */
 
-void empilha(Pilha* pi, int dados, Pilha* fim){
+void empilha(Pilha* l, Pilha* pi, int dados, Pilha* fim){
   Pilha* novo;
-  novo = cria_slot();
+  novo = cria_slot(l);
 	if(novo != NULL){
 		novo->val = dados;
 		pi->prox = novo;
@@ -59,7 +53,7 @@ void empilha(Pilha* pi, int dados, Pilha* fim){
 
 /* Remoção em pilhas é sempre realizado no início da pilha. OBS.: Não se pode remover de uma pilha vazia! Devemos verificar antes se a pilha está vazia */
 
-void desempilha(Pilha* pi, Pilha* fim){
+void desempilha(Pilha* l, Pilha* pi, Pilha* fim){
   if(pilha_vazia(pi)){
     fim->ant->prox = NULL; // O próximo do fim->ant
     fim = fim->ant;
