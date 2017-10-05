9eec650888d7884d8e1b7c9c6522f6dfe37e09b2
arthuevangelista <arthuevangelista@gmail.com>
2017-10-04 22:56:22 -0300

    Finalizado lista de exercícios sobre pilhas

--- 03 - Filas e Pilhas/PILHAS_03.c
+++ 03 - Filas e Pilhas/PILHAS_03.c
@@ -4,61 +4,23 @@
 
 #define MAX_BASES 300
 
-/*  switch(fi->fim){
-    case 'B':
-      // par será S
-      break;
-    case 'C':
-      // par será F
-      break;
-    case 'S':
-      // par será B
-      break;
-    case 'F':
-      // par será C
-      break;
-    default:
-      // caso inválido
-  }
-*/
-
-// conta o número de bases
-// divide no meio
-// realiza o pareamento
-
-// se não for par,
-
-void dobra(char* pilha){
-  int M; int i;
-  M = strlen(pilha);
-  int topp, t;
-
-  topp = 0; t = M;
-  char* aux = malloc( (M/2) * sizeof(char));
-
-  for(i = M; i != (M/2); i--){
-    aux[topp++] = pilha[i]; // Empilha os valores de cima da pilha para aux
-    pilha[--t]; // desempilha i
-  }
-
-}
+void dobra(char* pilha, int* t, int* contagem); // Protótipo de função
+void verifica(char* pilha, char* aux, int* topp, int* t, int* cont);  // Protótipo de função
 
 void ler_nome(char *s){
 // Função ler o nome
     char c;
-    //int i;
     c = getchar();
     while(c!='\n'){
         *s = c;
         c=getchar();
         s++;
-     //   i++;
     }
 }
 
 int main(){
   char s[MAX_BASES];
-  int i, topo;
+  int i, t, contagem;
   int N;
 
   printf("Insira as bases do RNAA: \n");
@@ -72,12 +34,87 @@ int main(){
     pilha[t++] = s[i]; // Insere o vetor S na pilha
   }
 
-  if(N%2 != 0){
-    pilha[--t]; // Verifica se é ímpar. Se for, retira o último item da pilha
+  dobra(pilha, &t, &contagem); // dobra t em um auxiliar e retorna o novo valor do topo da pilha
+
+  printf("O número de pares são: %d", contagem);
+
+  free(pilha);
+  return 0;
+}
+
+/*----------------------------------------------------*/
+/*                FUNÇÃO VERIFICA                     */
+/*----------------------------------------------------*/
+/* Recebe duas pilhas e verifica o pareamento do      */
+/* topo delas até que encontre um não pareamento      */
+/* Quando ocorrido o não pareamento, envia cada pilha */
+/* para a função dobra                                */
+/*----------------------------------------------------*/
+
+void verifica(char* pilha, char* aux, int* topp, int* t, int* cont){
+
+  while(*t != 1){
+    switch(pilha[*t]){
+      case 'B':
+        if(aux[*topp] == 'S'){
+          *cont++; // soma contagem
+          --*t; // desempilha pilha
+          --*topp; // desempilha auxiliar
+          break;
+        }
+      case 'C':
+        if(aux[*topp] == 'F'){
+          *cont++;
+          --*t;
+          --*topp;
+          break;
+        }
+      case 'S':
+        if(aux[*topp] == 'B'){
+          *cont++;
+          --*t;
+          --*topp;
+          break;
+        }
+      case 'F':
+        if(aux[*topp] == 'C'){
+          *cont++;
+          --*t;
+          --*topp;
+          break;
+        }
+      default:
+        dobra(pilha, t, cont);
+        dobra(aux, topp, cont);
+    }
+  }
+}
+
+/*----------------------------------------------*/
+/*                FUNÇÃO DOBRA                  */
+/*----------------------------------------------*/
+/* Recebe uma string, dobra esta string e manda */
+/* realizar o pareamento dessa pilha !          */
+/*----------------------------------------------*/
+
+void dobra(char* pilha, int* t, int* contagem){
+  int M; int i;
+  M = strlen(pilha);
+  int topp;
+
+  if(M%2 != 0){
+    --*t; // Verifica se é ímpar. Se for, retira o último item da pilha
   }
 
-printf("%d", N);
+  topp = 0;
+  char* aux = malloc( (M/2) * sizeof(char));
 
+  for(i = M; i != (M/2); i--){
+    aux[topp++] = pilha[i]; // Empilha os valores de cima da pilha para aux
+    pilha[--*t]; // desempilha i
+  }
 
-  return 0;
+  verifica(pilha, aux, &topp, t, contagem);
+
+  free(aux);
 }
