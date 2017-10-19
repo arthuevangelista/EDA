7ca6707105830457da250ab6b09c85bd22126392
Aluno <Jesus Cristo>
2017-10-19 09:54:02 -0200

    Terminamos

--- 04 - Arvores/arvore.c
+++ 04 - Arvores/arvore.c
@@ -34,7 +34,7 @@ arv* criar(char c, arv* sae, arv* sad){
 }
 
 int arv_vazia(arv* a){
-  return (a==NULL);
+  return(a==NULL);
 }
 
 arv* destroi(arv* a){
@@ -55,7 +55,7 @@ int consulta(arv* a, char c){
 
 void imprimirPreOrdem(arv* a){
 /* Imprime itens de uma árvore binária pré-definida em pré-ordem.*/
-  if(!arv_vazia){
+  if(!arv_vazia(a)){
     printf("%c ",a->info);
     imprimirPreOrdem(a->esq);
     imprimirPreOrdem(a->dir);
@@ -64,7 +64,7 @@ void imprimirPreOrdem(arv* a){
 
 void imprimirEmOrdem(arv* a){
   /* Imprime itens de uma árvore binária pré-definida em ordem.*/
-  if(!arv_vazia){
+  if(!arv_vazia(a)){
     imprimirEmOrdem(a->esq);
     printf("%c ",a->info);
     imprimirEmOrdem(a->dir);
@@ -74,7 +74,7 @@ void imprimirEmOrdem(arv* a){
 void imprimirPolonesa(arv* a){
   /* Imprime itens de uma árvore binária pré-definida em pós-ordem.
   Esta impresão também é conhecida como notação Polonesa*/
-  if(!arv_vazia){
+  if(!arv_vazia(a)){
     imprimirPolonesa(a->esq);
     imprimirPolonesa(a->dir);
     printf("%c ",a->info);
@@ -84,42 +84,60 @@ void imprimirPolonesa(arv* a){
 void imprimirLargura(char* fila, arv* a){
   /* Imprime itens de uma árvore binária pré-definida em largura.
   Insere os nós da árvore em uma fila e vai imprimindo elas de acordo. */
-  if(!arv_vazia){
-    fila = a->info;
-    imprimirLargura(a->esq);
-    imprimirLargura(a->dir);
+  if(!arv_vazia(a)){
     printf("%c ",a->info);
+    fila++;
+    *fila = a->esq->info;
+    fila++;
+    *fila = a->dir->info;
+    imprimirLargura(fila,a->esq);
+    imprimirLargura(fila,a->dir);
+
   }
 }
 
-void insereFila(){
-
+char* criaFila(int pedra){
+  char* fila = (char*)malloc(sizeof(char) * pedra);
+  return fila;
 }
 
 int main(){
-  arv *a,*b,*c,*d,*e,*f;
-  int opcao = 1;
+  arv *a = NULL,*b,*c,*d,*e,*f;
+  int opcao = 1; char op = 'z';
+  char* pedreiro;
+  int pedra = 0;
 
   while(opcao){
 
+    system("clear");
     printf("\t\t>> MENU <<\n\n");
     printf("0 - SAIR\n");
     printf("1 - Inserir arvore(pre definida)\n");
     printf("2 - Destrói árvore\n");
     printf("3 - Busca\n");
-    printf("4 - Impressão\n");
-    printf("5 - Impressão em largura\n");
+    printf("4 - Impressão pré-ordem\n");
+    printf("5 - Impressão em ordem\n");
+    printf("6 - Impressão pós-ordem (polonesa)\n");
+    printf("7 - Impressão em largura\n");
     printf("\t\t\t\tOPÇÃO: ");
+    setbuf(stdin, NULL);
+    scanf("%d", &opcao);
 
     switch(opcao){
       case 1:
         if(arv_vazia(a)){
           f = criar('f',NULL,NULL);
+          pedra++;
           e = criar('e',NULL,NULL);
+          pedra++;
           d = criar('d',NULL,NULL);
+          pedra++;
           b = criar('b',NULL,d);
+          pedra++;
           c = criar('c',e,f);
+          pedra++;
           a = criar('a',b,c);
+          pedra++;
         }
         break;
       case 2:
@@ -130,7 +148,19 @@ int main(){
           printf("\nEncontrei\n");
         break;
       case 4:
-        imprimir(a);
+        imprimirPreOrdem(a);
+        break;
+      case 5:
+        imprimirEmOrdem(a);
+        break;
+      case 6:
+        imprimirPolonesa(a);
+        break;
+      case 7:
+        pedreiro = criaFila(pedra);
+        *pedreiro = a->info;
+        imprimirLargura(pedreiro,a);
+        free(pedreiro);
         break;
     }
   }
